ccb64bb29845bd6da854cf9ea165b57ee3ac69eb
Naoyuki Saka <nao@Naoyukis-MacBook-Pro.local>
2022-04-24 17:18:11 +0900

    Firt commit

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from '@/app/home';
+
+export const App = () => (
+	<BrowserRouter>
+		<Routes>
+			<Route path="/" element={<Home />} />
+		</Routes>
+	</BrowserRouter>
+)
